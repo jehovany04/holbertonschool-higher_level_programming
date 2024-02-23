24443c994a63a36dd53950c52d52ebc92e7b3010
jehovany04 <7528@holbertonstudents.com>
2024-02-23 08:26:48 +0000

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,6 +23,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter method for width"""
+        self.validate_positive_integer("width", value)
         self.__width = value
 
     @property
@@ -33,6 +34,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter method for height"""
+        self.validate_positive_integer("height", value)
         self.__height = value
 
     @property
@@ -43,6 +45,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter method for x"""
+        self.validate_non_negative_integer("x", value)
         self.__x = value
 
     @property
@@ -53,5 +56,20 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter method for y"""
+        self.validate_non_negative_integer("y", value)
         self.__y = value
+
+    def validate_positive_integer(self, name, value):
+        """Validate that value is a positive integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def validate_non_negative_integer(self, name, value):
+        """Validate that value is a non-negative integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value < 0:
+            raise ValueError("{} must be >= 0".format(name))
 
