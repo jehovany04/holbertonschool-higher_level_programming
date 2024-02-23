c0008365a16f766ba0ad09fc9a014dbd9992516c
jehovany04 <7528@holbertonstudents.com>
2024-02-23 08:17:44 +0000

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module for the Rectangle class"""
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class that inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle instance"""
+
+        super().__init__(id)  # Call the constructor of the Base class with id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y"""
+        self.__y = value
+
