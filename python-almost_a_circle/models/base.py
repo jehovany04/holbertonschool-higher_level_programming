8c453a60e184504c2097f50c86dc74d03316582b
jehovany04 <7528@holbertonstudents.com>
2024-02-23 09:01:54 +0000

    square.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,3 +25,15 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dict))
+
