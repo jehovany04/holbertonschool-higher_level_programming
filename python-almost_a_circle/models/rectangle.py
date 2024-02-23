84534f3f547f08e5eb00baf10c9b0cdaa5afa372
jehovany04 <7528@holbertonstudents.com>
2024-02-23 09:11:22 +0000

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,26 @@
-# Dans models/rectangle.py
+#!/usr/bin/python3
+"""Module for the Rectangle class"""
+
+from models.base import Base
 
 class Rectangle(Base):
-    # ... (autres méthodes et propriétés)
+    """Rectangle class that inherits from Base"""
 
-    @classmethod
-    def create(cls, **dictionary):
-        """Crée une instance de Rectangle à partir d'un dictionnaire"""
-        new_rect = cls(1, 1)  # Les valeurs par défaut peuvent être modifiées
-        new_rect.update(**dictionary)
-        return new_rect
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle instance"""
+
+        super().__init__(id)  # Call the constructor of the Base class with id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
 
